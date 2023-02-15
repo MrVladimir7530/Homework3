61ac89cd7762a0701d3dda0c29215d5d95f58cf8
MrVladimir <vladimir.volkov-2013@yandex.ru>
2023-02-15 15:43:53 +0300

    Homework3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        float variable1 = 27.12F;
+        long variable2 = 987_678_965_549L;
+        double variable3 = 2.786;
+        short variable4 = 569;
+        short variable5 = -159;
+        int variable6 = 27897;
+        byte variable7 = 67;
+
+        System.out.println("вы написали число: " + variable1);
+        System.out.println("вы написали число: " + variable2);
+        System.out.println("вы написали число: " + variable3);
+        System.out.println("вы написали число: " + variable4);
+        System.out.println("вы написали число: " + variable5);
+        System.out.println("вы написали число: " + variable6);
+        System.out.println("вы написали число: " + variable7);
+        System.out.println("Так как домашка на самопроверке, то я не вижу смысла дальше делать");
     }
 }
